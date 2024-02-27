8adde98b7ae438480a469837311f841c479f68f6
胡峻阁 <1805824390@qq.com>
2024-02-27 18:44:16 +0800

    <feat>：公共字段填充自定义注解、自定义切面类部分实现

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,44 @@
+package com.sky.aspect;
+
+import com.sky.annotation.AutoFill;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义切面类，实现公共字段自动填充的处理逻辑
+ * */
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+    /**
+     * 切入点
+     */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointCut(){}
+
+    /**
+     * 前置通知，在通知中进行公共字段赋值
+     */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint){
+        log.info("开始进行公共字段填充...");
+        // 获取当前被拦截方法的数据库操作类型 U/I
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature(); // 方法签名对象
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class); // 获得方法上的注解对象
+        OperationType operationType = autoFill.value(); // 获得数据库操作类型
+
+        // 获取当前被拦截方法的参数--实体对象
+
+        // 准备赋值数据
+
+        // 根据不同的操作类型进行对应的赋值（通过反射）
+    }
+}
