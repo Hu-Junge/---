e2def62cf2ff4268350900d1636e870613ae8bf7
胡峻阁 <1805824390@qq.com>
2024-03-10 13:57:51 +0800

    <feat>：定时处理订单

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,17 +1,16 @@
 package com.sky.controller.user;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("userOrderController")
 @RequestMapping("/user/order")
@@ -32,5 +31,18 @@ public class OrderController {
         log.info("用户订单信息：{}",ordersSubmitDTO);
         OrderSubmitVO orderSubmitVO =  orderService.submitOrder(ordersSubmitDTO);
         return Result.success(orderSubmitVO);
+    }
+    /**
+     * 订单支付
+     */
+    @PutMapping("/payment")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO){
+        OrderPaymentVO orderPaymentVO = new OrderPaymentVO();
+        orderPaymentVO.setNonceStr("2024-03-24T14:15:22Z");
+        orderPaymentVO.setPaySign("111");
+        orderPaymentVO.setTimeStamp("111");
+        orderPaymentVO.setSignType("111");
+        orderPaymentVO.setPackageStr("111");
+        return Result.success(orderPaymentVO);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -3,6 +3,11 @@ package com.sky.mapper;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -11,4 +16,19 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+    /**
+     * 超时查询
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from sky_take_out.orders where status=#{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTime(Integer status, LocalDateTime orderTime);
+
+    /**
+     * 更新订单
+     * @param order
+     */
+    void update(Orders order);
 }
